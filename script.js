48e69edadbba86ab319a4db76db4926ac3c1b2da
Saad <260048854+saadahmadd@users.noreply.github.com>
2026-07-08 10:01:20 +0500

    Create class for making boxes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let canvas = document.querySelector('canvas')
 
 canvas.width = window.innerWidth
@@ -5,9 +7,7 @@ canvas.height = window.innerHeight
 
 let c = canvas.getContext('2d')
 
-let x
-let y
-let rectWidth = 100
+let rectWidth;
 let rectHeight = 100
 
 let divisionCount = 10
@@ -16,11 +16,27 @@ let division = window.innerWidth / divisionCount
 let halfDivision = division / 2;
 let temp = halfDivision;
 
+console.log(division)
+
+class Box {
+    constructor(height, width, color, amount) {
+        this.height = height
+        this.width = width
+        this.color = color
+    }
+
+    draw(iterations) {
+        c.beginPath()
+        c.strokeStyle = 'red';
+        c.strokeRect((division * iterations + halfDivision) - this.width/2, innerHeight - this.height, this.width, this.height)
+    }
+}
+
+const boxes = new Box(rectHeight, division)
+
 // note: rectangles start x-axis from the left and y-axis from the top, not the center
-for (i = 0; i < divisionCount; i++) {
-    c.beginPath()
-    c.fillRect((division * i + halfDivision) - rectWidth/2, innerHeight - rectHeight, rectWidth, rectHeight)
-    c.fill()
+for (let i = 0; i < divisionCount; i++) {
+    boxes.draw(i)
 }
 
 
