dbfad1816a13adb11e298ae148dc3f859ff9c53d
Saad <260048854+saadahmadd@users.noreply.github.com>
2026-07-08 10:16:59 +0500

    Add randomization of colors to boxes

--- script.js
+++ script.js
@@ -10,32 +10,37 @@ let c = canvas.getContext('2d')
 let rectWidth;
 let rectHeight = 100
 
-let divisionCount = 10
-let division = window.innerWidth / divisionCount 
+let boxCount = 20
+let division = window.innerWidth / boxCount 
 
 let halfDivision = division / 2;
 let temp = halfDivision;
 
-console.log(division)
+function randomColor() {
+    let r = Math.floor(Math.random() * 255)
+    let g = Math.floor(Math.random() * 255)
+    let b = Math.floor(Math.random() * 255)
+    return `rgb(${r}, ${g}, ${b})`
+}
 
 class Box {
-    constructor(height, width, color, amount) {
+    constructor(height, width) {
         this.height = height
         this.width = width
-        this.color = color
     }
 
     draw(iterations) {
         c.beginPath()
-        c.strokeStyle = 'red';
-        c.strokeRect((division * iterations + halfDivision) - this.width/2, innerHeight - this.height, this.width, this.height)
+        c.fillStyle = randomColor()
+        c.fillRect((division * iterations + halfDivision) - this.width/2, innerHeight - this.height, this.width, this.height)
+        c.fill()
     }
 }
 
 const boxes = new Box(rectHeight, division)
 
 // note: rectangles start x-axis from the left and y-axis from the top, not the center
-for (let i = 0; i < divisionCount; i++) {
+for (let i = 0; i < boxCount; i++) {
     boxes.draw(i)
 }
 
